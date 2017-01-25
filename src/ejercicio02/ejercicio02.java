1112d7a1c77b753a55ce20f050f503430c60fe32
1dam.m <1dam.m@AULA521.cenecmalaga.net>
2017-01-25 13:25:35 +0100

    ejercicio 02 terminado

--- src/ejercicio02/ejercicio02.java
+++ src/ejercicio02/ejercicio02.java
@@ -5,32 +5,36 @@ import java.util.Scanner;
 public class ejercicio02 {
 
 	public static void main(String[] args) {
-		
-		// 2. Programa que lee por teclado el valor del radio 
-		// de una circunferencia y calcula y muestra por 
+
+		// 2. Programa que lee por teclado el valor del radio
+		// de una circunferencia y calcula y muestra por
 		// pantalla la longitud y el área de la circunferencia.
-		float radio=0;
-		float area=0;
-		System.out.println("La longitud de este círculo es de: "+longitud(radio));
-		System.out.println("El área de este cículo es de : "+area(radio));
+		float radio = 0;
 		
+		radio = leerFloat();
+		
+		System.out.println("La longitud de este círculo es de: " + longitud(radio));
+		System.out.println("El área de este cículo es de : " + area(radio));
+
 	}
-public static float longitud(float radio){
-	float radiocir=0;
-	double longitud=0;
-	Scanner entrada=new Scanner (System.in);
-	System.out.print("Escribe el radio de la circunferencia: ");
-	radiocir=entrada.nextFloat();
-	longitud=2*radiocir*Math.PI;
-	return (float) longitud;
-}
-public static float area (float radio){
-	float radiocirculo=0;
-	double area=0;
-	Scanner entrada=new Scanner (System.in);
-	System.out.print("Escribe el radio de la circunferencia: ");
-	radiocirculo=entrada.nextFloat();
-	area=Math.PI*(radiocirculo*radiocirculo);
-	return (float) area;
-}
+	
+	public static float leerFloat(){
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Escribe el radio de la circunferencia: ");
+		float radio = entrada.nextFloat();
+		return radio;
+	}
+
+	public static float longitud(float radio) {
+		float radiocir = 0;
+		float longitud = 0;
+		longitud = (float) (2 * radio * Math.PI);
+		return longitud;
+	}
+
+	public static float area(float radio) {
+		double area = 0;
+		area = Math.PI * (radio * radio);
+		return (float) area;
+	}
 }
